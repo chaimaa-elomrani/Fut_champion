e96d8fa8fa676f65dfa52ce03d6a0efc56975054
chaima <chaimaelomrani6@gmail.com>
2025-11-12 09:57:28 +0100

    first version of front end completed

---
+++ index.js
@@ -0,0 +1,22 @@
+ function openAddPlayerModal() {
+            document.getElementById('addPlayerModal').classList.add('show');
+        }
+
+        function closeAddPlayerModal() {
+            document.getElementById('addPlayerModal').classList.remove('show');
+        }
+
+        function addPlayer(event) {
+            event.preventDefault();
+            alert('Player added successfully!');
+            closeAddPlayerModal();
+            event.target.reset();
+        }
+
+        // Close modal when clicking outside
+        window.onclick = function(event) {
+            const modal = document.getElementById('addPlayerModal');
+            if (event.target === modal) {
+                modal.classList.remove('show');
+            }
+        }
